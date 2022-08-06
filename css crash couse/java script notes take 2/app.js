d86f43e48f3464cdf9c05a500e0b75ee01d68628
unknown951258563465335 <89399889+unknown951258563465335@users.noreply.github.com>
2022-08-06 07:34:30 -0700

    main

--- css crash couse/java script notes take 2/app.js
+++ css crash couse/java script notes take 2/app.js
@@ -1,11 +1,8 @@
-
-let addBtn = document.querySelector('#addBtn');
-addBtn.addEventListener('click', addNotes);
-showNotes()
+// functions start
 function addNotes() {
     let textArea = document.querySelector('#textArea');
     let notes = localStorage.getItem('notes');
-    if (textArea.value.lenght != 0) {
+    if (textArea.value.length != 0) {
         if (notes == null) {
             notesArray = [];
         }
@@ -18,7 +15,7 @@ function addNotes() {
         showNotes()
     }
 }
-function showNotes(){
+function showNotes() {
     let notes = localStorage.getItem('notes');
     if (notes == null) {
         notesArray = [];
@@ -27,31 +24,54 @@ function showNotes(){
         notesArray = JSON.parse(notes)
     }
     let html = ''
-
-    notesArray.forEach(function(element , index) {
+    notesArray.forEach(function (element, index) {
         html += `
                         <div class="cards-border">
-        <h3>Note ${index+1}</h3>
+        <h3>Note ${index + 1}</h3>
         <p>${element}</p>
         <button onclick="deleteNote(${index})" id="${index}">Delete Notes</button>
     </div>`
     });
-    if(notesArray.length != 0){
+    if (notesArray.length != 0) {
         document.querySelector('#cards').innerHTML = html
     }
-    else{
+    else {
         document.querySelector('#cards').innerHTML = `<h1>There is Nothing to show here!</h1>`
     }
 }
 function deleteNote(index) {
     let notes = localStorage.getItem('notes')
-    if(notes == null){
+    if (notes == null) {
         notesArray = [];
     }
-    else{
+    else {
         notesArray = JSON.parse(notes)
     }
-    notesArray.splice(index,1)
+    notesArray.splice(index, 1)
     localStorage.setItem('notes', JSON.stringify(notesArray));
     showNotes()
 }
+
+function searchNotes() {
+    let cards = document.getElementsByClassName('cards-border')
+    Array.from(cards).forEach(function (element) {
+        let content = element.getElementsByTagName('p')[0].innerText;
+        if (content.includes(search.value.toLowerCase())) {
+            element.style.display = 'block'
+        }
+        else {
+
+            element.style.display = 'none'
+        }
+    });
+}
+
+// functions end
+
+
+let addBtn = document.querySelector('#addBtn');
+let search = document.getElementById('search')
+addBtn.addEventListener('click', addNotes);
+search.addEventListener('input', searchNotes)
+showNotes()
+
